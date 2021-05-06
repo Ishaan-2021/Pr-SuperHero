d97da5d25f9a7a323ffb50d8bdbef2e68b5100a3
Ishaan-2021 <76939917+Ishaan-2021@users.noreply.github.com>
2021-05-06 13:10:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ block_image_height=30;
 player_x=10;
 player_y=10;
 var player_object="";
+var block_image_object="";
 function player_update()
 {
     fabric.Image.fromURL("player.png",function(Img){
